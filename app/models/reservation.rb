d027b6d3a492ec6cb968b83dda19f6187c47efeb
Martin Sanchez <msanchez9@uc.cl>
2021-11-20 22:47:08 -0300

    fix model asociation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,4 @@ class Reservation < ApplicationRecord
     belongs_to :room
     belongs_to :hour
     belongs_to :movie
-    has_many :seats
 end
--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,5 +1,4 @@
 class Seat < ApplicationRecord
   belongs_to :room
   belongs_to :hour
-  belongs_to :reservation
 end
